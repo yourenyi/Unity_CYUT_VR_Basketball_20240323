e51f7578bc3d57b259ec1126f48999fcf44be588
yourenyi <164702261+yourenyi@users.noreply.github.com>
2024-03-24 16:32:45 +0800

    update:生成籃球

--- Assets/scripts/BasketballSystem.cs
+++ Assets/scripts/BasketballSystem.cs
@@ -1,8 +1,14 @@
-
+﻿
 using UnityEngine;
 
 public class BasketballSystem : MonoBehaviour
 {
     [SerializeField]
     private GameObject basketball;
+    public void SpawnBasketball()
+    {
+        Instantiate(basketball,transform.position, Quaternion.identity);
+
+
+    }
 }
