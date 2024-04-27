55d7a7d713873e361ef6c2f9a75be4de113635e0
yourenyi <164702261+yourenyi@users.noreply.github.com>
2024-04-27 15:42:11 +0800

    update設置重新開始跟退出遊戲

---
+++ Assets/scripts/GameManger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManger : MonoBehaviour
+{
+   public void Replay()
+    {
+        SceneManager.LoadScene("Äx²y³õ");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
