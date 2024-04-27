0bfe6501c44c118d29b22e3e6c254d71d2f22ca8
yourenyi <164702261+yourenyi@users.noreply.github.com>
2024-04-27 11:29:58 +0800

    0427分數設計完成

---
+++ Assets/scripts/ScoreManger.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreManger : MonoBehaviour
+{
+    [SerializeField, Header("分數文字")]
+    private TMP_Text testScore;
+    private string basketballName = "籃球";
+    private int score;
+        private void OnTriggerEnter(Collider other)
+    {
+        // print(other.name);
+        if (other.name.Contains(basketballName))
+        {
+            score += 2;
+            // print($"<color=#f93>分數:{score}</color>");
+            testScore.text = $"score - {score}";
+        }
+
+    }
+}
