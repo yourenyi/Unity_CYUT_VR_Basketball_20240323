b8e0ebc07312411e7b1447df40e084547099cb80
yourenyi <164702261+yourenyi@users.noreply.github.com>
2024-04-27 15:27:19 +0800

    update音效設定進籃跟生成

--- Assets/scripts/ScoreManger.cs
+++ Assets/scripts/ScoreManger.cs
@@ -5,6 +5,11 @@ public class ScoreManger : MonoBehaviour
 {
     [SerializeField, Header("分數文字")]
     private TMP_Text testScore;
+    [SerializeField, Header("音效來源")]
+    private AudioSource aud;
+    [SerializeField, Header("進籃音效")]
+    private AudioClip soundBasketball;
+
     private string basketballName = "籃球";
     private int score;
         private void OnTriggerEnter(Collider other)
@@ -15,6 +20,7 @@ public class ScoreManger : MonoBehaviour
             score += 2;
             // print($"<color=#f93>分數:{score}</color>");
             testScore.text = $"score - {score}";
+            aud.PlayOneShot(soundBasketball);
         }
 
     }
